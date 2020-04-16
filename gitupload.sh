5c520b373f64c3876de15cd7ff4e7d46b51cc43c
magnuscloud <arivazhagan.vp@gmail.com>
2020-04-16 23:44:00 +0530

    updated

---
+++ gitupload.sh
@@ -0,0 +1,14 @@
+echo "=> git fetch"
+git fetch
+git pull
+echo "=> git status"
+git status
+echo "=> git add ."
+git add .
+echo "=> git commit -m "updated""
+git commit -m "updated"
+echo "=> git push"
+git push
+echo "=> git status"
+git status
+read -p "Press any key to continue... " -n1 -s
---
+++ php-sample-app.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Congratulations. Your PHP testing is successful</h1>
+
+<?php
+echo "Hello World!<br><br>";
+echo "Version: v1";
+?> 
+
+</body>
+</html>
